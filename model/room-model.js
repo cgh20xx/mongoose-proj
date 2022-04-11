16dcdf009a7888dfa8c7b70f66511a8ca8b8ec78
Hank-Ad2-MBP16 <hank@ad2iction.com>
2022-04-11 17:09:18 +0800

    重構 將 RoomModel 模組化

---
+++ model/room-model.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+// 2. 建立 room schema (簡易版)
+/* 
+const roomSchema = {
+  name: String,
+  price: {
+    type: Number,
+    required: [true, '價格必填'],
+  },
+  rating: Number,
+};
+*/
+
+// 2. 建立 room schema (完整版)
+const roomSchema = new mongoose.Schema(
+  {
+    name: String,
+    price: {
+      type: Number,
+      required: [true, '價格必填'],
+    },
+    rating: Number,
+    // 若不使用內建 timestamps: true，也可自定 createAt 規則。
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      select: false, // false 表不顯示此欄位。 .find() 查不出來
+    },
+  },
+  {
+    versionKey: false,
+    // collection: 'rooms', // 亦可直接寫死 collection 名字，不受預設小寫及結尾s影響。
+    // timestamps: true, // mongoose 會自動新增 createdAt 和 updatedAt 欄位。
+  }
+);
+
+// 3. 建立 room model
+const RoomModel = mongoose.model('Room', roomSchema);
+/* 
+  注意：
+  mongoose 會將 'Room' 轉換為 mongodb collection 的 'rooms'。
+  所有字母強制小寫。
+  結尾強制加 s ，若結尾已有 s 則不會加。
+*/
+module.exports = RoomModel;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const mongoose = require('mongoose');
+const RoomModel = require('./model/room-model');
 const db = 'hotel';
 
 // 1. 連接資料庫
@@ -9,50 +10,6 @@ mongoose
     console.log('mongodb 連接成功');
   })
   .catch((error) => console.log(error));
-
-// 2. 建立 room schema (簡易版)
-/* 
-const roomSchema = {
-  name: String,
-  price: {
-    type: Number,
-    required: [true, '價格必填'],
-  },
-  rating: Number,
-};
-*/
-
-// 2. 建立 room schema (完整版)
-const roomSchema = new mongoose.Schema(
-  {
-    name: String,
-    price: {
-      type: Number,
-      required: [true, '價格必填'],
-    },
-    rating: Number,
-    // 若不使用內建 timestamps: true，也可自定 createAt 規則。
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      select: false, // false 表不顯示此欄位。 .find() 查不出來
-    },
-  },
-  {
-    versionKey: false,
-    // collection: 'rooms', // 亦可直接寫死 collection 名字，不受預設小寫及結尾s影響。
-    // timestamps: true, // mongoose 會自動新增 createdAt 和 updatedAt 欄位。
-  }
-);
-
-// 3. 建立 room model
-const RoomModel = mongoose.model('Room', roomSchema);
-/* 
-  注意：
-  mongoose 會將 'Room' 轉換為 mongodb collection 的 'rooms'。
-  所有字母強制小寫。
-  結尾強制加 s ，若結尾已有 s 則不會加。
-*/
 
 // 4. 新增資料方法1：建立 RoomModel 實例並使用 save() 將資料寫入 mongodb
 /* 
@@ -74,7 +31,7 @@ roomDoc
 
 // 4. 新增資料方法2：使用 create()
 RoomModel.create({
-  name: '豪華蜜月套房1',
+  name: '豪華蜜月套房 module',
   price: 7000,
   rating: 4.8,
 })
